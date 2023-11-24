06a86e8f16351b443312d4443d915c6ef2cc0bf4
hehedewuyu <13566353761@163.com>
2023-11-24 22:59:09 +0800

    删除图片爬虫外的其他爬虫

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from image import crawlImage
-from video import crawlHamineVideo
-import requests
-import commImage
-import os
-import tkinter as tk
-from tkinter import *
-from menu import show_menu
+#from menu import show_menu
 
 if __name__ == '__main__':
-    show_menu()
-    '''type = input("请输入爬取类型:")
-    if 'p' == type:
-        crawlImage()
-    elif 'v' == type:
-        crawlHamineVideo()'''
+    #show_menu()
+    
+    crawlImage()
 
 
 
