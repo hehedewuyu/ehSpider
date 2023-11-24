d26d4e7fa225b98ba596abd4083ccfd12f5b0b4a
hehedewuyu <13566353761@163.com>
2023-11-24 22:44:44 +0800

    删除全部与图片爬虫不相关的文件

--- commVideo.py
+++
@@ -1,8 +0,0 @@
-g_headers = {
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Upgrade-Insecure-Requests': '1'
-}
-
-g_imageRoot = 'D:\\yellow\\视频\\'
-g_reCheckDownload = {}
